c295c94510dc897c65d4ddec37068c5b36b14935
Thusan Arul <piruthusan@gmail.com>
2024-11-27 21:43:02 +0100

    Adds boilerplate for token stuff in scanner

--- lox/src/scanner.rs
+++ lox/src/scanner.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub struct Scanner {
     source: Vec<u8>,
 }
@@ -15,4 +17,76 @@ impl Scanner {
 }
 
 #[derive(Debug)]
-pub struct Token;
+enum Literal {
+    Number(f64),
+    String(String),
+    // Probably other stuff?
+}
+
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Literal,
+    line: i32,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {:?} {:?}",
+            self.token_type, self.lexeme, self.literal
+        )
+    }
+}
+
+#[derive(Debug)]
+enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
