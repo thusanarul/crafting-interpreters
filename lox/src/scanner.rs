81e2999e8fef3c7f3438ab169b53d644dbbfe72e
Thusan Arul <piruthusan@gmail.com>
2024-12-11 21:36:44 +0100

    Moves token to own file

--- lox/src/scanner.rs
+++ lox/src/scanner.rs
@@ -1,7 +1,7 @@
-use std::fmt::Display;
-
 use phf::phf_map;
 use thiserror::Error;
+
+use crate::token::{Literal, Token, TokenType};
 
 #[derive(Error, Debug, Clone)]
 pub(crate) enum Error {
@@ -199,6 +199,7 @@ impl Scanner {
         }
     }
 
+    // Identifiers could denote variables, types, labels, subroutines and packages.
     fn identifier(&mut self) -> Token {
         while self.is_alphanumeric(self.peek()) {
             self.advance();
@@ -338,91 +339,4 @@ impl Scanner {
         self.current = self.current + 1;
         return true;
     }
-}
-
-#[derive(Debug, Clone)]
-pub enum Literal {
-    Number(f64),
-    String(String),
-    // Probably other stuff?
-}
-
-#[derive(Debug, Clone)]
-pub struct Token {
-    token_type: TokenType,
-    lexeme: String,
-    literal: Option<Literal>,
-    line: i32,
-}
-
-impl Token {
-    pub fn new(token_type: TokenType, lexeme: String, literal: Option<Literal>, line: i32) -> Self {
-        Self {
-            token_type,
-            lexeme,
-            literal,
-            line,
-        }
-    }
-}
-
-impl Display for Token {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{:?} {:?} {:?}",
-            self.token_type, self.lexeme, self.literal
-        )
-    }
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum TokenType {
-    // Single-character tokens.
-    LeftParen,
-    RightParen,
-    LeftBrace,
-    RightBrace,
-    Comma,
-    Dot,
-    Minus,
-    Plus,
-    Semicolon,
-    Slash,
-    Star,
-
-    // One or two character tokens.
-    Bang,
-    BangEqual,
-    Equal,
-    EqualEqual,
-    Greater,
-    GreaterEqual,
-    Less,
-    LessEqual,
-
-    // Literals.
-    Identifier,
-    String,
-    Number,
-
-    // Keywords.
-    And,
-    Class,
-    Else,
-    False,
-    Fun,
-    For,
-    If,
-    Nil,
-    Or,
-    Print,
-    Return,
-    Super,
-    This,
-    True,
-    Var,
-    While,
-
-    Eof,
 }
---
+++ lox/src/token.rs
@@ -0,0 +1,86 @@
+#[derive(Debug, Clone)]
+pub enum Literal {
+    Number(f64),
+    String(String),
+    // Probably other stuff?
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<Literal>,
+    line: i32,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, literal: Option<Literal>, line: i32) -> Self {
+        Self {
+            token_type,
+            lexeme,
+            literal,
+            line,
+        }
+    }
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:?} {:?} {:?}",
+            self.token_type, self.lexeme, self.literal
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
